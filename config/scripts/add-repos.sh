8aec23d0491543160273a32f4bc81b7649df7d11
Pierce Talbert <piercetalbert@outlook.com>
2026-01-12 08:53:33 -0600

    update recipe

--- config/scripts/add-repos.sh
+++ config/scripts/add-repos.sh
@@ -23,25 +23,6 @@ gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 
-# Terra (Discord)
-curl -Lo /etc/yum.repos.d/terra.repo https://repos.fyralabs.com/terra\$releasever.repo
 
-# Negativo17 (Spotify) - using fedora-spotify ID manually
-cat > /etc/yum.repos.d/fedora-spotify.repo <<EOF
-[fedora-spotify]
-name=negativo17 - Spotify
-baseurl=https://negativo17.org/repos/spotify/fedora/\$releasever/\$basearch/
-enabled=1
-skip_if_unavailable=1
-gpgkey=https://negativo17.org/repos/RPM-GPG-KEY-slaanesh
-gpgcheck=1
-enabled_metadata=1
-metadata_expire=6h
-type=rpm-md
-repo_gpgcheck=0
-EOF
-
-# Heroic Games Launcher (COPR) - using atim repo which is currently maintained
-curl -Lo /etc/yum.repos.d/atim-heroic-games-launcher-fedora-41.repo https://copr.fedorainfracloud.org/coprs/atim/heroic-games-launcher/repo/fedora-41/atim-heroic-games-launcher-fedora-41.repo
 
 echo "Repositories added successfully."
